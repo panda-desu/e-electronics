07ffc3bd97c4fb2f1cb336127af542b2a9758e2c
Binderiya <binderiyatsogthers@gmail.com>
2023-03-31 14:06:13 +0800

    test commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import "./styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// test
+
 root.render(
   <BrowserRouter>
     <React.StrictMode>
